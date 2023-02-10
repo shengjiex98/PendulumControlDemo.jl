ae0830cfe18cdc44be578eab468fc3103abe2031
Clara Hobbs <clara@clarahobbs.com>
2023-02-10 12:07:20 -0500

    Add GLMakie pendulum skeleton
    There's a simple drawing of a pendulum, but it isn't moving the way a
    pendulum should.  Still, this gives an example of how to draw a pendulum
    with Observables, how to animate it, and how to allow interaction with a
    slider.

--- src/PendulumControlDemo.jl
+++ src/PendulumControlDemo.jl
@@ -1,6 +1,5 @@
 module PendulumControlDemo
 
-# Write your package code here.
 export pendulum_run
 include("pendulum.jl")
 
--- src/pendulum.jl
+++ src/pendulum.jl
@@ -1,94 +1,43 @@
-using SimpleDirectMediaLayer
-using SimpleDirectMediaLayer.LibSDL2
+using GLMakie
 
-function pendulum_run()
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 16)
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16)
+"""
+    pendulum_run(;fps=60)
 
-    @assert SDL_Init(SDL_INIT_EVERYTHING) == 0 "error initializing SDL: $(unsafe_string(SDL_GetError()))"
+Run a pendulum simulation at the given framerate.
+"""
+function pendulum_run(;fps=60)
+    fig = Figure()
+    ax = Axis(fig[1, 1], aspect=1)
+    limits!(ax, [-1.5, 1.5], [-1.5, 1.5])
 
-    win = SDL_CreateWindow("Control Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_SHOWN)
-    SDL_SetWindowResizable(win, SDL_TRUE)
+    θ = draw_pendulum!(ax)
 
-    renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
+    sl = Slider(fig[2, 1], range=-1:0.01:1, startvalue=0)
 
-    surface = IMG_Load(joinpath("src", "cat.png"))
-    tex = SDL_CreateTextureFromSurface(renderer, surface)
-    SDL_FreeSurface(surface)
-
-    w_ref, h_ref = Ref{Cint}(0), Ref{Cint}(0)
-    SDL_QueryTexture(tex, C_NULL, C_NULL, w_ref, h_ref)
-
-    try
-        w, h = w_ref[], h_ref[]
-        pos = [(1000 - w) ÷ 2, (1000 - h) ÷ 2]
-        dest_ref = Ref(SDL_Rect(pos..., w, h))
-        close = false
-        speed = 300
-        dirs = [0, 0]
-        while !close
-            event_ref = Ref{SDL_Event}()
-            while Bool(SDL_PollEvent(event_ref))
-                evt = event_ref[]
-                if evt.type == SDL_QUIT
-                    close = true
-                    break
-                elseif evt.type == SDL_KEYDOWN
-                    scan_code = evt.key.keysym.scancode
-                    if scan_code == SDL_SCANCODE_W || scan_code == SDL_SCANCODE_UP
-                        dirs[2] -= 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_A || scan_code == SDL_SCANCODE_LEFT
-                        dirs[1] -= 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_S || scan_code == SDL_SCANCODE_DOWN
-                        dirs[2] += 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_D || scan_code == SDL_SCANCODE_RIGHT
-                        dirs[1] += 1
-                        break
-                    else
-                        break
-                    end
-                elseif evt.type == SDL_KEYUP
-                    scan_code = evt.key.keysym.scancode
-                    if scan_code == SDL_SCANCODE_W || scan_code == SDL_SCANCODE_UP
-                        dirs[2] += 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_A || scan_code == SDL_SCANCODE_LEFT
-                        dirs[1] += 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_S || scan_code == SDL_SCANCODE_DOWN
-                        dirs[2] -= 1
-                        break
-                    elseif scan_code == SDL_SCANCODE_D || scan_code == SDL_SCANCODE_RIGHT
-                        dirs[1] -= 1
-                        break
-                    else
-                        break
-                    end
-                end
-            end
-
-            dirs = max.(dirs, [-1, -1])
-            dirs = min.(dirs, [1, 1])
-            pos += speed / 30 * dirs
-            pos = max.(pos, [0, 0])
-            pos = min.(pos, [1000 - w, 1000 - h])
-
-            dest_ref[] = SDL_Rect(pos..., w, h)
-            SDL_RenderClear(renderer)
-            SDL_RenderCopy(renderer, tex, C_NULL, dest_ref)
-            dest = dest_ref[]
-            pos[1], pos[2], w, h = dest.x, dest.y, dest.w, dest.h
-            SDL_RenderPresent(renderer)
-
-            SDL_Delay(1000 ÷ 60)
+    on(events(fig.scene).window_open) do event
+        event || return
+        @async while isopen(fig.scene)
+            θ[] += sl.value[] / 10
+            sleep(1/fps)
         end
-    finally
-        SDL_DestroyTexture(tex)
-        SDL_DestroyRenderer(renderer)
-        SDL_DestroyWindow(win)
-        SDL_Quit()
     end
+
+    fig
+end
+
+"""
+    draw_pendulum!(ax::Axis)
+
+Draw a unit length pendulum on the given axis.  Returns θ, the angle of the pendulum from
+vertical.
+"""
+function draw_pendulum!(ax::Axis)
+    θ = Observable(0.0)
+    pend = @lift(Point2f(sin($θ), -cos($θ)))
+    points = @lift([Point2f(0, 0), $pend])
+
+    lines!(ax, points)
+    poly!(ax, @lift(Circle($pend, 0.1)))
+
+    θ
 end
