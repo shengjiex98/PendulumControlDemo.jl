d4cf15bedc57866bbdfbc050bf2cd6060f8b06e7
Clara Hobbs <clara@clarahobbs.com>
2023-02-20 11:02:04 -0500

    Add pendulum dynamics model
    Based on the pendulum model from
    https://alsaibie.github.io/me417/numerical_lessons_julia/Numerical_Lesson_1/index.html

--- src/pendulum.jl
+++ src/pendulum.jl
@@ -10,14 +10,21 @@ function pendulum_run(;fps=60)
     ax = Axis(fig[1, 1], aspect=1)
     limits!(ax, [-1.5, 1.5], [-1.5, 1.5])
 
-    θ = draw_pendulum!(ax)
+    x = Observable([0.0, 0.0])
+    θ = draw_pendulum!(ax, x)
 
     sl = Slider(fig[2, 1], range=-1:0.01:1, startvalue=0)
 
     on(events(fig.scene).window_open) do event
         event || return
         @async while isopen(fig.scene)
-            θ[] += sl.value[] / 10
+            mr = 3; l = .19; g = 9.81; ; b = 0.1;
+            I = 4/3*mr*l^2; 
+            dx = [0.0; 0.0]
+            dx[1] = x[][2];
+            u = sl.value[]
+            dx[2] = (u - mr * g * l * sin(x[][1]) - b * x[][2] ) / (I + mr * l^2);
+            x[] = x[] + dx * 1/fps
             sleep(1/fps)
         end
     end
@@ -31,8 +38,8 @@ end
 Draw a unit length pendulum on the given axis.  Returns θ, the angle of the pendulum from
 vertical.
 """
-function draw_pendulum!(ax::Axis)
-    θ = Observable(0.0)
+function draw_pendulum!(ax::Axis, x)
+    θ = @lift($x[1])
     pend = @lift(Point2f(sin($θ), -cos($θ)))
     points = @lift([Point2f(0, 0), $pend])
 
